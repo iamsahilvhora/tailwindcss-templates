2106ca55ec5b6755fc85b6d669e337776136f962
Sahil <iamsahilvhora@gmail.com>
2019-06-15 15:13:04 +0530

    fair price v1

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  theme: {
+    fontFamily: {
+      'sans': 'Inter, -apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Helvetica Neue, Arial, Noto Sans, sans-serif, Apple Color Emoji, Segoe UI Emoji, Segoe UI Symbol, Noto Color Emoji',
+    },
+    extend: {
+      screens: {
+        xxl: '1440px',
+      },
+    },
+  },
+  variants: {},
+  plugins: [],
+}
